4b6f7e96d5d3296081ef28125b683cc24863d401
MarioMz99 <72156134+MarioMz99@users.noreply.github.com>
2021-02-10 12:56:55 -0800

    asd

--- src/Principal.java
+++ src/Principal.java
@@ -3,5 +3,11 @@ public class Principal {
     public static void main(String[] args){
         System.out.println("Hola mundo");
         System.out.println("bye");
+        pantalla();
+
+    }
+
+    public static void pantalla(){
+        System.out.println("Jeje");
     }
 }
